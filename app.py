9502abb888fa4aae39c3db3d5cbfc538801f8992
Alexander Dich Jensen <dichjensen@gmail.com>
2024-05-22 22:33:58 +0200

    Renaming

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import aws_cdk as cdk
 
 from infra.stacks.remote_stack import Remote
-from infra.stacks.shared_stack import WebServiceStack
+from infra.stacks.web_service_stack import WebServiceStack
 
 app = cdk.App()
 
